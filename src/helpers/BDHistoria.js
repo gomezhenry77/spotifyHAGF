47e8ef30bcab1504df3fabc6b0a67d6754b73f3d
407 <407@academicos.cesde.edu.co>
2022-09-29 16:34:49 -0500

    se crea selección de artistas

---
+++ src/helpers/BDHistoria.js
@@ -0,0 +1,13 @@
+let artistas=[
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-9b3a0.appspot.com/o/bannerColdplay.jpg?alt=media&token=931c9273-78c5-400f-9f89-11f55cad1850',
+    historia:'Coldplay es una banda británica de pop rock y rock alternativo formada en Londres en 1996'},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+]
---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,6 @@
+const TOKEN="Bearer BQA7NXeRDU3pyAuf_FroCqq1F3nVKKIaiK-_MdHbpDBA7urt_GSn9Xa-RH2NwX5jSTX6oTmKWBCAtcWXVm8nIMErI71cjiRJYddO-jCGImNawMqahqnOSHz-puosZHwKNV-T0J9OgI8Ff3CrJ8hEXiZ1fNJd1lCl1Mz8KOUu4m-Uy2JqNuXbtrL5BpWY8Ss"
+
+export const PETICION={
+    method:"GET",
+    headers:{Authorization:TOKEN},
+}
